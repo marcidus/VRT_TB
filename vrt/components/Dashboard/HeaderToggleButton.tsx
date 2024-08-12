7fe972dfb7c493bc7b7b4b06fdbe30efba46023b
marcidus <amj@mac3.ch>
2024-08-12 22:52:48 +0200

    toggle button for the sensors update (on/off)

--- vrt/components/Dashboard/HeaderToggleButton.tsx
+++ vrt/components/Dashboard/HeaderToggleButton.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { DataService } from '../Data/DataService';
+import { FaPause, FaSearch } from 'react-icons/fa'; // Import appropriate icons
+import gsap from 'gsap';
 import './HeaderToggleButton.css'; // Import the CSS file
 
-// Props for the HeaderToggleButton component
 interface HeaderToggleButtonProps {
   headersUpdated: boolean; // The current state of header updates
   toggleHeadersUpdated: (newState: boolean) => void; // Function to toggle the state of header updates
@@ -11,7 +12,6 @@ interface HeaderToggleButtonProps {
 const HeaderToggleButton: React.FC<HeaderToggleButtonProps> = ({ headersUpdated, toggleHeadersUpdated }) => {
   const [initialHeadersUpdated, setInitialHeadersUpdated] = useState(false);
 
-  // Fetch the initial state of headersUpdated from the server when the component mounts
   useEffect(() => {
     console.log('Fetching initial headersUpdated state');
     const dataService = DataService.getInstance();
@@ -24,7 +24,6 @@ const HeaderToggleButton: React.FC<HeaderToggleButtonProps> = ({ headersUpdated,
       .catch(error => console.error('Error fetching headersUpdated state:', error));
   }, [toggleHeadersUpdated]);
 
-  // Handle the toggle button click event
   const handleToggle = () => {
     console.log('Toggling headersUpdated state');
     const dataService = DataService.getInstance();
@@ -32,18 +31,24 @@ const HeaderToggleButton: React.FC<HeaderToggleButtonProps> = ({ headersUpdated,
       .then(data => {
         console.log('Received toggled headersUpdated state:', data);
         toggleHeadersUpdated(data);
+
+        // Animate toggle button with GSAP
+        gsap.to('.toggle-knob', {
+          x: data ? 26 : 0,
+          backgroundColor: data ? '#28a745' : '#dc3545',
+          duration: 0.3,
+        });
       })
       .catch(error => console.error('Error toggling headersUpdated state:', error));
   };
 
   return (
-    <div className="header-toggle-button-container">
-      <button
-        onClick={handleToggle}
-        className={`header-toggle-button ${headersUpdated ? 'bg-green' : 'bg-red'}`}
-      >
-        {headersUpdated ? 'Disable Header Updates' : 'Enable Header Updates'}
-      </button>
+    <div className="header-toggle-button-container" onClick={handleToggle}>
+      <div className={`toggle-wrapper ${headersUpdated ? 'active' : ''}`}>
+        <div className="toggle-knob">
+          {headersUpdated ? <FaSearch color="#fff" /> : <FaPause color="#fff" />}
+        </div>
+      </div>
     </div>
   );
 };
